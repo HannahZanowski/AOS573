90711747df9a64296e249a9c1e0342d35af90015
Hannah Zanowski <zanowski@wisc.edu>
2021-10-04 11:01:32 -0500

    fixed an error in another_dot_product.py ; imported numpy so that the function can be read in as a module

--- Homework/another_dot_product.py
+++ Homework/another_dot_product.py
@@ -1,6 +1,8 @@
+import numpy as np
+
 def another_dot_product(vec1,vec2):
     mult=[]
-    for i in range(0,len()):
+    for i in range(0,len(vec1)):
         mult.append(vec1[i]*vec2[i]) 
     dot=np.sum(mult)
     return(dot)    
