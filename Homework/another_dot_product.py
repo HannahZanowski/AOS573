0fcd41413ee89eb88b35294ab6dd6854274e98e2
Hannah Zanowski <zanowski@wisc.edu>
2022-09-10 17:57:12 -0500

    Shift ClassNotebook2 (numpy and matplotlib to CN1)

--- Homework/another_dot_product.py
+++ Homework/another_dot_product.py
@@ -1,6 +1,10 @@
 import numpy as np
 
 def another_dot_product(vec1,vec2):
+#This function is an example of how to
+#compute a dot product
+#INPUTS: vec1 and vec2 (1D lists, arrays)
+#RETURNS: the dot product of 2 1D vectors
     mult=[]
     for i in range(0,len(vec1)):
         mult.append(vec1[i]*vec2[i]) 
