7f216d293daa6387c1e6ce4c616aa398198c69d6
Hannah Zanowski <zanowski@wisc.edu>
2022-10-21 17:36:26 -0500

    renaming make_cartopy_axis_circular module and moving contents

--- Lectures/make_cartopy_axis_circular/setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup
-
-setup(name = "make_ax_circular",
-    version = "0.0.1",
-    author = "Hannah Zanowski",
-    packages=['make_ax_circular'],
-    install_requires=['numpy','matplotlib','cartopy'])
